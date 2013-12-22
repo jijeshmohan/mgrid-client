2f53a682ffd64957d755088d901ebd334afae34b
Jijesh Mohan <jijeshmohan@gmail.com>
2013-12-22 13:33:37 +0530

    getting task from server

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ var socket = io.connect(config.serverUrl);
  socket.on('connect', function(){
  	console.log("Connecting Device");
  	socket.emit('deviceInfo', config.device);
+ 	socket.on("execute",function(data){
+ 		console.log(data.runitem)
+ 		console.log("Running tests.......");
+ 		socket.emit("status",{runitem: data.runitem,status: 'Running'})
+ 	});
     socket.on('disconnect', function(){
     	console.log("Disconnected");
     });
