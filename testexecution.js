ef69b4fa5acf7cf95dc0d2a889baca09dd710713
Jijesh Mohan <jijeshmohan@gmail.com>
2014-02-03 23:17:43 +0530

    calabash android tests

--- testexecution.js
+++ testexecution.js
@@ -6,12 +6,14 @@ var path = require('path'),
 exports.run = function(socket,id,config){
   if(config.type && config.type==="mvn"){
       maven_run(socket,id,config);
-   }else{
+   }else if(config.type==="android"){
+     android_run(socket,id,config);
+   }else {
      cucumber_run(socket,id,config);
    }
 };
 
-function cucumber_run(socket,id,config){
+function android_run(socket,id,config){
    var report_folder = new Date().getTime();
       var environment = process.env;
       var path = __dirname + "/" + config.reportDir + "/" + report_folder;
@@ -22,7 +24,7 @@ function cucumber_run(socket,id,config){
         environment[key]=config.environment[key];
       }
 
-      var command = 'cucumber -d -f json -o '+ path +'/result.json';
+      var command = 'calabash-android run '+ config.apk +' -f json -o '+ path +'/result.json';
 
       var options = {
         cwd: config.projectPath,
@@ -55,10 +57,37 @@ function maven_run(socket,id,config){
 
       child = exec(command, options,function(error, stdout, stderr) {
             if (error !== null) {
-                console.log("Error while execution");
-                socket.emit('result',{result: {},id: id});
+                console.log("Error while execution" + error);
+                // socket.emit('result',{result: {},id: id});
             }
             var result = require(config.projectPath +'/target/cucumber-json-report.json');
+            socket.emit('result',{result: result,id: id});
+      });
+}
+
+function cucumber_run(socket,id,config){
+   var report_folder = new Date().getTime();
+      var environment = process.env;
+      var path = __dirname + "/" + config.reportDir + "/" + report_folder;
+
+      mkdirp.sync(path);
+
+      for(key in config.environment){
+        environment[key]=config.environment[key];
+      }
+
+      var command = 'cucumber -d -f json -o '+ path +'/result.json';
+
+      var options = {
+        cwd: config.projectPath,
+        env: environment
+      };
+      child = exec(command, options,function(error, stdout, stderr) {
+            if (error !== null) {
+              console.log("Error while execution");
+                socket.emit('result',{result: {},id: id});
+            }
+            var result = require(path +'/result.json');
             socket.emit('result',{result: result,id: id});
       });
 }
